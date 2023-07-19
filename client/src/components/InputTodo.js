7afb9d8fc6b57b19fcd77461a8d97ee9c7b09321
Haleynp <haleynpatel@gmail.com>
2023-07-19 17:46:50 -0400

    File restructuring

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -41,7 +41,6 @@ const InputTodo = ({progress, done}) => {
 
     return(
         <>
-        <h1 className='text-center mt-5'> Todo List </h1>
         <form className = "d-flex mt-5" onSubmit={onSubmitForm}>
              <input type="text" className="form-control" value = {description} onChange = {e => setDescription(e.target.value)}/>
              <button className = "btn btn-success">Add</button>
